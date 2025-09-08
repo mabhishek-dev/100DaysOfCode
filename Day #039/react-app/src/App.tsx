a53ff58da1379c93b56dc304efcb7878fbd9c816
M Abhishek <abhishekmuniswamy444@gmail.com>
2025-09-08 23:34:25 +0530

    Add files via upload

---
+++ Day #039/react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import Message from "./Message";
+
+function App() {
+  return (
+    <div>
+      {/*<Message></Message> This line can also be written as the below*/}
+      <Message />
+    </div>
+  );
+}
+
+export default App;
